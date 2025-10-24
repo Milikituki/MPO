e3bb0edb0d39ab198eed30dc1f034d363d4520c6
Milikituki <myriam1869@gmail.com>
2025-10-24 22:44:28 +0200

    ejercicio 3

--- Sesion2/Ejercicios/Ejercicios/src/Ejercicio3.java
+++ Sesion2/Ejercicios/Ejercicios/src/Ejercicio3.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Introduce el número de segundos que quieres convertir: ");
-        int segundos = s.nextInt();
-        //horas - segundos -> *3600
-        //horas - minutos -> *60
-        //minutos - segundos -> *60
-        int minutos = (segundos%3600)/60;
-        int horas = segundos/3600;
-        int segundosRestantes = segundos%60;
-        System.out.printf("""
-                %d hora(s), %d minuto(s) y %d segundo(s)
-                """, horas, minutos, segundosRestantes);
+        System.out.println("Decenas de mil: ");
+        String decenaMil = s.next();
+        System.out.println("Unidades de mil: ");
+        String unidadesMil = s.next();
+        System.out.println("Centenas: ");
+        String centenas = s.next();
+        System.out.println("Decenas: ");
+        String decenas = s.next();
+        System.out.println("Unidades: ");
+        String unidades = s.next();
+
+        String num = decenaMil+unidadesMil+centenas+decenas+unidades;
+        System.out.println(num);
+
+//        int num = decenaMil*10000 + unidadesMil*1000 + centenas*100 + decenas*10 + unidades;
+//        System.out.println("Numero introducido: "+num);
+
+
+
     }
+
+
 }
