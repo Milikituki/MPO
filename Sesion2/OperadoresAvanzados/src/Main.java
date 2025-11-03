7f0de5493790aba32880c92934bbff1d4e1d7f5d
Miriam Fernández <myriam1869@gmail.com>
2025-11-03 09:29:04 +0100

    Recolector de basura y casteo implícito

--- Sesion2/OperadoresAvanzados/src/Main.java
+++ Sesion2/OperadoresAvanzados/src/Main.java
@@ -8,6 +8,18 @@ public class Main {
         GestorDocumentos gD = new GestorDocumentos();
         gU.validarUsuario("Borja", 31, "borja@gmail.com");
 
+        //------------------------------------------//
+
+        String palabraCifrar = "Esta frase la quiero cifrar";
+        for (int i = 0; i < palabraCifrar.length(); i++) {
+            int codigo = (int)palabraCifrar.charAt(i);
+            if (i%2==0){
+                codigo*=2;
+            }else {codigo*=3;}
+            System.out.print(codigo+" ");
+
+        }
+
 
 
     }
