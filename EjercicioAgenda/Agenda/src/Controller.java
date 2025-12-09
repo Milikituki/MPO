d54dadc9d6b8b7db9cfe18474e2bd941f6687136
Miriam Fernández <myriam1869@gmail.com>
2025-12-09 12:13:09 +0100

    feat agenda completo

--- EjercicioAgenda/Agenda/src/Controller.java
+++ EjercicioAgenda/Agenda/src/Controller.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Controller {
     Scanner scanner = new Scanner(System.in);
-    ArrayList<Object> agenda = new ArrayList<>();
+    ArrayList<Object[]> agenda = new ArrayList<>();
     public void mostrarMenu(){
         int opcion;
         do {
@@ -21,15 +22,15 @@ public class Controller {
 
             switch (opcion){
                 case 1 -> agregarPersona(agenda);
-                case 2 -> buscarPersona();
-                case 3 -> borrarPersona();
-                case 4 -> listarPersonas();
+                case 2 -> buscarPersona(agenda);
+                case 3 -> borrarPredicado(agenda);
+                case 4 -> listarPersonas(agenda);
                 case 5 -> System.out.println("Saliendo...");
                 default -> System.out.print("Opción inválida. ");
             }
         }while(opcion != 5);
     }
-    private void agregarPersona(ArrayList<Object> listaContactos){
+    private void agregarPersona(ArrayList<Object[]> listaContactos){
         System.out.print("Nombre: ");
         String nombreContacto = scanner.next();
         System.out.print("Apellido: ");
@@ -38,15 +39,70 @@ public class Controller {
         int telefonoContacto = scanner.nextInt();
         System.out.print("DNI: ");
         String dniContacto = scanner.next();
-        listaContactos.add(new Object[]{nombreContacto, apellidoContacto, telefonoContacto, dniContacto});
+        listaContactos.add(new Object[]{nombreContacto, apellidoContacto, telefonoContacto, dniContacto.toUpperCase()});
     }
-    private void buscarPersona(){
+    private void buscarPersona(ArrayList<Object[]> listaContactos){
+        System.out.print("Introduce el DNI a buscar: ");
+        String dniBuscar = scanner.next();
+        for (int i = 0; i < listaContactos.size(); i++) {
+            Object[] contacto = listaContactos.get(i);
+            for (int j = 0; j < contacto.length; j++) {
+                if (contacto[3].equals(dniBuscar.toUpperCase())){
+                    System.out.printf("""
+                            🧑🏼‍🦰: %s %s
+                            ☎️: %d
+                            💳: %s
+                            
+                            """, contacto[0], contacto[1], (int)contacto[2], contacto[3]);
+                    break;
+                } else{
+                    System.out.println("No se han encontrado coincidencias.");
+                    break;
+                }
+            }
+        }
 
     }
-    private void borrarPersona(){
+    private void borrarPersona(ArrayList<Object[]> listaContactos){
+        System.out.print("Indica el DNI a borrar: ");
+        String dniBorrar = scanner.next();
 
+        for (int i = 0; i < listaContactos.size(); i++) {
+            Object[] contacto = listaContactos.get(i);
+            if (contacto[3].equals(dniBorrar.toUpperCase())){
+                listaContactos.remove(i);
+                System.out.println("Contacto borrado correctamente.");
+                break;
+            } else{
+                System.out.println("No se ha podido borrar el contacto, inténtelo de nuevo.");
+                break;
+            }
+
+        }
     }
-    private void listarPersonas(){
+    private void listarPersonas(ArrayList<Object[]> listaContactos){
+        for (Object[] contacto : listaContactos) {
+            for (Object item : contacto) {
+                System.out.printf("""
+                        🧑🏼‍🦰: %s %s
+                        ☎️: %d
+                        💳: %s
+                        
+                        """, contacto[0], contacto[1], (int)contacto[2], contacto[3]);
+                break;
+            }
+        }
+    }
+    private void borrarPredicado(ArrayList<Object[]> listaContactos){
+        System.out.print("Introduce el DNI del contacto que deseas borrar: ");
+        String dniBorrar = scanner.next();
+            boolean borrado = listaContactos.removeIf(contacto -> contacto[3].equals(dniBorrar.toUpperCase()));
+            if (borrado){
+                System.out.println("Contacto borrado correctamente.");
+            }else {
+                System.out.println("No se pudo borrar el contacto.");
+            }
+
 
     }
 }
